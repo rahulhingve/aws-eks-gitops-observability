cf78d8d16736df01a71c759904103a297070ae75
rahulhingve <rahulpawar2001.rp@gmail.com>
2026-04-02 21:27:12 +0530

    Update EKS cluster version to 1.30

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,7 @@ variable "cluster_name" {
 variable "cluster_version" {
     description = "EKS Cluster Version"
     type = string
-    default = "1.29"
+    default = "1.30"
 }
 
 variable "vpc_cider" {
