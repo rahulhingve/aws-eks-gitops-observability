0be5c4f646e89cdf3adec8637efdb4c4c5cc4287
rahulhingve <rahulpawar2001.rp@gmail.com>
2026-04-02 22:50:30 +0530

    Rename variable 'vpc_cider' to 'vpc_cidr'

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,7 +5,7 @@ data "aws_availability_zones" "available" {
 ####VPC####
 
 resource "aws_vpc" "main" {
-    cidr_block = var.vpc_cider
+    cidr_block = var.vpc_cidr
     enable_dns_hostnames = true
     enable_dns_support = true
 
@@ -31,7 +31,7 @@ resource "aws_internet_gateway" "main" {
 resource "aws_subnet" "public" {
     count = 2
     vpc_id = aws_vpc.main.id
-    cidr_block = cidrsubnet(var.vpc_cider, 8, count.index)
+    cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index)
     availability_zone = data.aws_availability_zones.available.names[count.index]
     map_public_ip_on_launch = true
     
