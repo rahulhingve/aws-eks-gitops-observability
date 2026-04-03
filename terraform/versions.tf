5afaf3ea16f948de0ab421e49239db653a068e32
rahulhingve <rahulpawar2001.rp@gmail.com>
2026-04-03 15:19:37 +0530

    Format versions.tf for better readability

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,18 +1,18 @@
 terraform {
-    required_version = ">= 1.6"
+  required_version = ">= 1.6"
 
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 5.0"
-        }
-        kubernetes = {
-            source = "hashicorp/kubernetes"
-            version = "~> 2.23"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23"
+    }
+  }
 }
 
 provider "aws" {
-    region = var.aws_region
+  region = var.aws_region
 }
