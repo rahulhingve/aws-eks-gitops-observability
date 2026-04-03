791b1d717a7ca48c4e2003db69efb969dad6226c
rahulhingve <rahulpawar2001.rp@gmail.com>
2026-04-03 15:17:36 +0530

    Format outputs.tf for consistency

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,16 +1,16 @@
 output "cluster_name" {
-    value = aws_eks_cluster.main.name
-  
+  value = aws_eks_cluster.main.name
+
 }
 
 output "cluster_endpoint" {
-    value = aws_eks_cluster.main.endpoint
+  value = aws_eks_cluster.main.endpoint
 }
 
 output "cluster_version" {
-    value = aws_eks_cluster.main.version
+  value = aws_eks_cluster.main.version
 }
 
 output "region" {
-    value = var.aws_region
+  value = var.aws_region
 }
