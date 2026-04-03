42e14132c2b7a9c10ffc671b0af6ea4192c08026
rahulhingve <rahulpawar2001.rp@gmail.com>
2026-04-03 15:20:28 +0530

    Refactor VPC configuration for better readability

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,62 +1,62 @@
 data "aws_availability_zones" "available" {
-    state = "available"
+  state = "available"
 }
 
 ####VPC####
 
 resource "aws_vpc" "main" {
-    cidr_block = var.vpc_cidr
-    enable_dns_hostnames = true
-    enable_dns_support = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-    tags = {
-        Name = "${var.cluster_name}-vpc"
-        "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    }
+  tags = {
+    Name                                        = "${var.cluster_name}-vpc"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
 }
 
 #### Internet Gateway ####
 
 resource "aws_internet_gateway" "main" {
-    vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-    tags = {
-        Name = "${var.cluster_name}-igw"
-    }
+  tags = {
+    Name = "${var.cluster_name}-igw"
+  }
 }
 
 
 # public subnets
 
 resource "aws_subnet" "public" {
-    count = 2
-    vpc_id = aws_vpc.main.id
-    cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index)
-    availability_zone = data.aws_availability_zones.available.names[count.index]
-    map_public_ip_on_launch = true
-    
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
 
-    tags = {
-        Name = "${var.cluster_name}-public-subnet-${count.index + 1}"
-        "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-        "kubernetes.io/role/elb" = "1"
-    }
+
+  tags = {
+    Name                                        = "${var.cluster_name}-public-subnet-${count.index + 1}"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
 }
 
 # private subnets  (for Worker Nodes)
 
 resource "aws_subnet" "private" {
-    count = 2
-    vpc_id = aws_vpc.main.id
-    cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index + 10)
-    availability_zone = data.aws_availability_zones.available.names[count.index]
-    
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 10)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 
-    tags = {
-        Name = "${var.cluster_name}-private-subnet-${count.index + 1}"
-        "kubernetes.io/cluster/${var.cluster_name}" = "owned"
-        "kubernetes.io/role/internal-elb" = "1"
-    }
+
+  tags = {
+    Name                                        = "${var.cluster_name}-private-subnet-${count.index + 1}"
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
 }
 
 
