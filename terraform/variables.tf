44c3219fd5d346f608a17e4edd5bf25025c64ee5
rahulhingve <rahulpawar2001.rp@gmail.com>
2026-04-03 15:18:48 +0530

    Refactor variable definitions in variables.tf
    Updated variable definitions for consistency in formatting and default values.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,44 +1,44 @@
 variable "aws_region" {
-    description = "AWS Region"
-    type = string
-    default = "ap-south-1"
+  description = "AWS Region"
+  type        = string
+  default     = "ap-south-1"
 }
 
 variable "cluster_name" {
-    description = "EKS Cluster Name"
-    type = string
-    default = "eks-gitops-observability"
+  description = "EKS Cluster Name"
+  type        = string
+  default     = "eks-gitops-observability"
 }
 
 variable "cluster_version" {
-    description = "EKS Cluster Version"
-    type = string
-    default = "1.30"
+  description = "EKS Cluster Version"
+  type        = string
+  default     = "1.30"
 }
 
 variable "vpc_cidr" {
-    description = "VPC CIDR Block"
-    type = string
-    default = "10.0.0.0/16"
+  description = "VPC CIDR Block"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "node_instance_type" {
-    description = "EC2 Instance type for worker nodes "
-    type = string
-    default = "t3.small"
+  description = "EC2 Instance type for worker nodes "
+  type        = string
+  default     = "t3.small"
 }
 
 variable "node_desired_count" {
-    type = number
-    default = 2
+  type    = number
+  default = 1
 }
 
 variable "node_min_count" {
-    type = number
-    default = 1
+  type    = number
+  default = 1
 }
 
 variable "node_max_count" {
-    type = number
-    default = 2
+  type    = number
+  default = 1
 }
