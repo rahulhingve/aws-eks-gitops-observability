aa46fdfcad02791ff422980254f4ed0f4c896c18
rahulhingve <rahulpawar2001.rp@gmail.com>
2026-04-05 17:22:03 +0530

    feat: update welcome message

--- app/index.js
+++ app/index.js
@@ -53,7 +53,7 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
     res.json({
-        message: "hello from aws eks demo app",
+        message: "hello from aws eks demo app v2",
         version: process.env.APP_VERSION || "1.0.0",
         hostname: os.hostname(),
 
